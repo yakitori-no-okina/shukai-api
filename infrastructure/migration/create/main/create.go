c7452dd27487f6dccf93cbeaca4c3ad60242df08
SoccerPlay32 <yahoosekainosekai@gmail.com>
2021-03-21 23:44:33 +0900

    setup migration

--- infrastructure/migration/create/main/create.go
+++ infrastructure/migration/create/main/create.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"fmt"
+	"os"
+	"shukai-api/domain"
+	"shukai-api/infrastructure"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Print("env load error")
+	}
+
+	dsn := os.Getenv("DATABASE_URL")
+	sqlHandler := infrastructure.NewSqlHandler(dsn)
+
+	sqlHandler.Conn.AutoMigrate(&domain.UserSkillModel{})
+	sqlHandler.Conn.AutoMigrate(&domain.RecruitmentConditionsModel{})
+
+	sqlHandler.Conn.AutoMigrate(&domain.UserModel{})
+	sqlHandler.Conn.AutoMigrate(&domain.NotificationModel{})
+	sqlHandler.Conn.AutoMigrate(&domain.RecruitmentModel{})
+	sqlHandler.Conn.AutoMigrate(&domain.ApprovalWaitModel{})
+	sqlHandler.Conn.AutoMigrate(&domain.RecruitmentUsersModel{})
+}
--- infrastructure/migration/drop/main/drop.go
+++ infrastructure/migration/drop/main/drop.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"fmt"
+	"os"
+	"shukai-api/infrastructure"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Print("env load error")
+	}
+
+	dsn := os.Getenv("DATABASE_URL")
+	sqlHandler := infrastructure.NewSqlHandler(dsn)
+
+	sqlHandler.Conn.Migrator().DropTable("notification_models")
+	sqlHandler.Conn.Migrator().DropTable("approval_wait_models")
+	sqlHandler.Conn.Migrator().DropTable("recruitment_users_models")
+	sqlHandler.Conn.Migrator().DropTable("recruitment_conditions_models")
+	sqlHandler.Conn.Migrator().DropTable("recruitment_models")
+	sqlHandler.Conn.Migrator().DropTable("user_skill_models")
+	sqlHandler.Conn.Migrator().DropTable("user_models")
+}
