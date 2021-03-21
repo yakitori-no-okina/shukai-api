9b2967eee368730bb58124fd7cbea4d9a4ec9ee4
SoccerPlay32 <yahoosekainosekai@gmail.com>
2021-03-21 23:44:15 +0900

    setup sqlhandler

--- infrastructure/sqlhandler.go
+++ infrastructure/sqlhandler.go
@@ -1,1 +1,56 @@
 package infrastructure
+
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type SqlHandler struct {
+	Conn *gorm.DB
+}
+
+func NewSqlHandler(dsn string) *SqlHandler {
+	conn, err := gorm.Open(postgres.Open(dsn))
+	if err != nil {
+		panic(err.Error())
+	}
+	sqlHandler := new(SqlHandler)
+	sqlHandler.Conn = conn
+	return sqlHandler
+}
+
+func (handler *SqlHandler) Find(out interface{}, where ...interface{}) *gorm.DB {
+	return handler.Conn.Find(out, where...)
+}
+
+func (handler *SqlHandler) First(out interface{}, where ...interface{}) *gorm.DB {
+	return handler.Conn.First(out, where...)
+}
+
+func (handler *SqlHandler) Exec(sql string, values ...interface{}) *gorm.DB {
+	return handler.Conn.Exec(sql, values...)
+}
+
+func (handler *SqlHandler) Raw(sql string, values ...interface{}) *gorm.DB {
+	return handler.Conn.Raw(sql, values...)
+}
+
+func (handler *SqlHandler) Create(value interface{}) *gorm.DB {
+	return handler.Conn.Create(value)
+}
+
+func (handler *SqlHandler) Save(value interface{}) *gorm.DB {
+	return handler.Conn.Save(value)
+}
+
+func (handler *SqlHandler) Delete(value interface{}) *gorm.DB {
+	return handler.Conn.Delete(value)
+}
+
+func (handler *SqlHandler) Where(query interface{}, args ...interface{}) *gorm.DB {
+	return handler.Conn.Where(query, args...)
+}
+
+func (handler *SqlHandler) Model(value interface{}) *gorm.DB {
+	return handler.Conn.Model(value)
+}
--- interfaces/database/sqlhandler_interface.go
+++ interfaces/database/sqlhandler_interface.go
@@ -1,1 +1,15 @@
 package database
+
+import "gorm.io/gorm"
+
+type SqlHandler interface {
+	Find(out interface{}, where ...interface{}) *gorm.DB
+	First(out interface{}, where ...interface{}) *gorm.DB
+	Exec(sql string, values ...interface{}) *gorm.DB
+	Raw(sql string, values ...interface{}) *gorm.DB
+	Create(value interface{}) *gorm.DB
+	Save(value interface{}) *gorm.DB
+	Delete(value interface{}) *gorm.DB
+	Where(query interface{}, args ...interface{}) *gorm.DB
+	Model(value interface{}) *gorm.DB
+}
